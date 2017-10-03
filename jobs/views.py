edf753b529a8cfcd32a7698c9f378d9f7823dcbb
Deepak Bhaskaran <deepak@duo.com>
2017-10-02 21:29:45 -0400

    Added pagination to the page displaying the list of jobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Job, Company
 
 # Create your views here.
 def job_list(request):
     jobs = Job.objects.all()
-    return render(request, 'jobs/job_list.html', { 'jobs': jobs })
+    paginator = Paginator(jobs, 4) # Show 4 jobs per page
+
+    page = request.GET.get('page')
+
+    try:
+        job_pages = paginator.page(page)
+    except PageNotAnInteger:
+        job_pages = paginator.page(1)
+    except EmptyPage:
+        job_pages = paginator.page(paginator.num_pages)
+
+    return render(request, 'jobs/job_list.html', { 'jobs': job_pages })
 
 def job_detail(request, pk):
     job = get_object_or_404(Job, pk=pk)
